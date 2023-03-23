290d113a03f0cfb237f5b8bd6592f8b74369a35e
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-03-23 22:52:26 +0200

    update task 2

--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -5,7 +5,7 @@ import css from './Statistic.module.css';
 export const StatisticsList = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.stat_list}>
         {stats.map(stat => (
